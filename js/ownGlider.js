c9a6c83e033663b49c631ead16aab0d8e385cade
Jose David <Davidherrera0429@gmail.com>
2022-03-12 15:52:27 -0500

    first carousel whit glider.js

---
+++ js/ownGlider.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carousel-list"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: ".carousel-indicators",
+    arrows: {
+      prev: ".carousel-previous",
+      next: ".carousel-next",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 800,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+    ]
+  });
+});
