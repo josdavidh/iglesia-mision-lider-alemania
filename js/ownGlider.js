55cf7281bc94f496140cca426302d5db44f8ff7d
Jose David <Davidherrera0429@gmail.com>
2022-03-13 18:30:16 -0500

    progess

--- js/ownGlider.js
+++ js/ownGlider.js
@@ -14,14 +14,14 @@ window.addEventListener("load", function () {
         settings: {
           // Set to `auto` and provide item width to adjust to viewport
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },{
         // screens greater than >= 1024px
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 2,
+          slidesToScroll: 1
         }
       }
     ]
