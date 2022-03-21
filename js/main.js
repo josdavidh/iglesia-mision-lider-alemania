e134b5613182b4907781fe03b893c3bc2f38ae17
Jose David <Davidherrera0429@gmail.com>
2022-03-21 18:09:09 -0500

    modificacion al responsive, nav main y footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-main").classList.toggle("show");
   });
+
+let navItems = document.querySelectorAll(".nav-main ul li a");
+  
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    console.log("hola")
+    document.querySelector(".nav-main").classList.toggle("show")});
+});
