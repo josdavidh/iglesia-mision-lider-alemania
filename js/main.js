fca44c3bf0d71daf08440b9242fb38e6f9f0b06b
Rodrigo Jimenez <70282402+Rodry-12@users.noreply.github.com>
2023-02-21 16:47:54 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-main").classList.toggle("show");
-  });
+// document.querySelector(".menu-btn").addEventListener("click", () => {
+//     document.querySelector(".nav-main").classList.toggle("show");
+//   });
 
-let navItems = document.querySelectorAll(".nav-main ul li a");
-  
+window.addEventListener('scroll', function () {
+  document.querySelector(".nav-main").classList.toggle("scroll", this.window.scrollY > 80);
+  console.log("Scroll");
+});
+
+// Menu desplegable
+var menu = document.getElementById('menu-btn');
+var navBar = document.querySelector('.nav-bar');
+var navItems = document.querySelectorAll('.nav-bar a')
+
+menu.addEventListener("click", () => {
+  navBar.classList.toggle("show");
+});
+
 navItems.forEach(item => {
   item.addEventListener("click", () => {
-    console.log("hola")
-    document.querySelector(".nav-main").classList.toggle("show")});
+  navBar.classList.toggle("show")});
 });
